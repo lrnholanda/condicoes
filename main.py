7d6f7fed02aa946fbe38b55c18f9c2d2abd9995a
lrnholanda <lrnholanda@gmail.com>
2021-03-10 21:43:02 +0000

    criei var para receber o valor correto caso a condicao for vdd

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 age = 12
 
 if age < 4:
-    print("Your admission cost is $0.")
+    price = 0
 elif age < 18:
-    print("your admission cost is $25.")
+    price = 25
 else:
-    print("Your admission cost is $40.")
+    price = 40
+print(f"Your admission cost is ${price}.")    
